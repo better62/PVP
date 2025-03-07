91dd3f69d62aad3f3e9ac321f7a02dc9a030d9d6
better62 <gold9962@g.skku.edu>
2025-03-07 16:00:52 +0900

    0305

--- PVP/sample_data.py
+++
@@ -1,9 +0,0 @@
-"""
-1) Messages: For each of the 20 topics, compiled 28-30 concrete and actionable target behaviors using GPT-4o
-2) Persuaion strategies: Five persuasion strategies (first four strategies into P/N frames)
-3) Premises: Three premises for each persuasion strategy using GPT-4o
-4) Queries: Converted each premise into a suitable prompt for DALLE and a search query for Google using GPT-4o
-5) Images: one image from DALLE and on from Google Image Search for each premise (54 images)
-6) Persuasive Scores: 0 to 10
-7) Personal Information: Annotator ID, Age, Gender, Habit. Psychological Characteristics (Big5, PVQ21, MFQ30)
-"""
